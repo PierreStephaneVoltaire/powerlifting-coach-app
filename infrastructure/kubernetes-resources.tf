128af4c35ff2a10a4732391f8e4e2c4799180acb
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-11-04 23:34:09 -0500

    updated password setup

--- infrastructure/kubernetes-resources.tf
+++ infrastructure/kubernetes-resources.tf
@@ -92,8 +92,8 @@ resource "kubernetes_secret" "app_secrets" {
   }
 
   data = {
-    database-url           = "postgresql://app_user:${random_password.postgres_password[0].result}@postgres:5432/powerlifting_app"
-    rabbitmq-url           = "amqp://admin:${random_password.rabbitmq_password[0].result}@rabbitmq:5672/"
+    database-url           = "postgresql://app_user:${urlencode(random_password.postgres_password[0].result)}@postgres:5432/powerlifting_app"
+    rabbitmq-url           = "amqp://admin:${urlencode(random_password.rabbitmq_password[0].result)}@rabbitmq:5672/"
     keycloak-client-secret = random_password.keycloak_client_secret[0].result
     spaces-key             = digitalocean_spaces_key.default.access_key
     spaces-secret          = digitalocean_spaces_key.default.secret_key
